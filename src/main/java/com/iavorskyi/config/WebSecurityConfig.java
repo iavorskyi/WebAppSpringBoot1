18536ffec852a8bfe7b9626f4a09018ec718b00c
Iavorskyi <a7038329@gmail.com>
2020-07-11 13:18:52 +0300

    Initial commit

---
+++ src/main/java/com/iavorskyi/config/WebSecurityConfig.java
@@ -0,0 +1,47 @@
+package com.iavorskyi.config;
+
+import com.iavorskyi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+//                .cors().disable()
+//                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/registration", "/img/**", "/static/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
+}
---
+++ src/main/java/com/iavorskyi/domain/Message.java
@@ -0,0 +1,76 @@
+package com.iavorskyi.domain;
+
+import javax.persistence.*;
+
+@Entity // This tells Hibernate to make a table out of this class
+@SuppressWarnings("JpaAttributeTypeInspection")
+public class Message {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String text;
+    private String tag;
+    private String filename;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    public Message() {
+    }
+    public Message(String text, String tag, User user) {
+        this.text = text;
+        this.tag = tag;
+        this.author = user;
+    }
+    public Message(String text, String tag, User author, String filename) {
+        this.text = text;
+        this.tag = tag;
+        this.author = author;
+        this.filename = filename;
+    }
+
+
+    public String getAuthorName (){
+        return author != null? author.getUsername() : "<none>";
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+}
