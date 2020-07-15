3e6a41f1c82e95433ded3c9560992ee613bc40e5
Iavorskyi <a7038329@gmail.com>
2020-07-15 15:51:11 +0300

    Добавленo шифрование паролей

--- src/main/java/com/iavorskyi/service/UserService.java
+++ src/main/java/com/iavorskyi/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -17,9 +18,10 @@ import java.util.stream.Collectors;
 public class UserService implements UserDetailsService {
     @Autowired
     UserRepo userRepo;
-
     @Autowired
     MailSender mailSender;
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     public boolean addUser(User user){
         User userFromDb = userRepo.findByUsername(user.getUsername());
@@ -28,11 +30,10 @@ public class UserService implements UserDetailsService {
         }
         user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
-
         user.setActivationCode(UUID.randomUUID().toString());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
 
         userRepo.save(user);
-
         sendMessage(user);
 
         return true;
